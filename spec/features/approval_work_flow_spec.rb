a481bcf8c9ab597fab01909c509744bc7a8f6e3e
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2020-02-24 19:01:43 -0800

    Itegrated permissions to hide status form element form non admins, rspec green, rubocop green

--- spec/features/approval_work_flow_spec.rb
+++ spec/features/approval_work_flow_spec.rb
@@ -4,17 +4,27 @@ describe 'navigate' do
   let(:admin) { create_logged_in_admin }
   let(:user) { create_logged_in_user }
 
-  before do
-    login(admin)
-    @post = Post.create!(date: Date.today, rationale: 'Post1', user: user)
-  end
-
   describe 'edit' do
-    it 'has a status that can be edited' do
+    before do
+      login(admin)
+      @post = Post.create!(date: Date.today, rationale: 'Post1', user: user)
+    end
+
+    it 'has a status that can be edited on the form by an admin' do
+      login(admin)
+
       visit edit_post_path(@post)
       choose('post_status_approved')
       click_on('Submit')
       expect(@post.reload.status).to eq('approved')
+    end
+
+    it 'cannot be edited by a non admin' do
+      logout(admin)
+      login(user)
+
+      visit edit_post_path(@post)
+      expect(page).to_not have_content('Approved')
     end
   end
 end
